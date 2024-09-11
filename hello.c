1a5780b36d007eef776edbde60ef965f6d79775b
renan ferreira <renanferreira1534@gmail.com>
2024-09-11 11:34:07 +0000

    first commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+// imporatando a biblioteca padrão de entrada e saida de dados
+// std(standard) i(input) o(output)
+#include <stdio.h>
+// inicio do programa.
+int main(){
+// comando para exibir o testo hello na tela
+// o comando printf só executa porque voce fez uma importação
+// da biblioteca stdio.
+        printf("hello world\n");
+// o comando abaix informa ao compilador que programa acabou
+        return 0;
+ 
+}
